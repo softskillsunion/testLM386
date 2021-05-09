69dca438682d584756b5d555daeef4621d6c5f10
軟實力精進聯盟 Soft Skills Union <softskillsunion@gmail.com>
2021-05-10 04:09:08 +0800

    map

--- testLM386.ino
+++ testLM386.ino
@@ -1,66 +1,94 @@
 #include <Adafruit_NeoPixel.h>
 int ledPin = D5;
 int soundVolume = 0;
-int lastSoundVolume = 0;
+int counter = 0;
+int times = 3;
 
 Adafruit_NeoPixel strip(1, ledPin, NEO_RGB + NEO_KHZ800);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  strip.begin();           
-  strip.show();           
+  strip.begin();
+  strip.show();
   strip.setBrightness(255);
 }
 
-void loop() {
-soundVolume = analogRead(A0);
-Serial.println(soundVolume);
-if (lastSoundVolume!=soundVolume) {
-if (soundVolume=0) {
-  strip.setPixelColor(0, strip.Color(0, 0, 0));
-    strip.show();
-}
-if (soundVolume>30 && soundVolume<=60) {
-  strip.setPixelColor(0, strip.Color(0, 100, 0));
-    strip.show();
-}
-if (soundVolume>60 && soundVolume<=90) {
-  strip.setPixelColor(0, strip.Color(0, 255, 0));
-    strip.show();
-}
-if (soundVolume>90 && soundVolume<=120) {
-  strip.setPixelColor(0, strip.Color(0, 100, 100));
-    strip.show();
-}
-if (soundVolume>120 && soundVolume<=150) {
-  strip.setPixelColor(0, strip.Color(0, 255, 255));
-    strip.show();
-}
-if (soundVolume>150 && soundVolume<=180) {
-  strip.setPixelColor(0, strip.Color(0, 0, 100));
-    strip.show();
-}
-if (soundVolume>180 && soundVolume<=210) {
-  strip.setPixelColor(0, strip.Color(0, 0, 255));
-    strip.show();
-}
-if (soundVolume>210 && soundVolume<=240) {
-  strip.setPixelColor(0, strip.Color(100, 0, 100));
-    strip.show();
-}
-if (soundVolume>240 && soundVolume<=270) {
-  strip.setPixelColor(0, strip.Color(255, 0, 255));
-    strip.show();
-}
-if (soundVolume>270 && soundVolume<=300) {
-  strip.setPixelColor(0, strip.Color(100, 0, 0));
-    strip.show();
-}
-if (soundVolume>300) {
-  strip.setPixelColor(0, strip.Color(255, 0, 0));
-    strip.show();
-}
-delay(80);
-}
-lastSoundVolume = soundVolume;
+void loop()
+{
+  soundVolume = analogRead(A0);
+  soundVolume = map(soundVolume, 0, 350, 0, 10); // soundVolume = map(soundVolume, 0, 1024, 0, 10);
+  if (soundVolume == 0)
+  {
+    counter++;
+    if (counter >= times)
+    {
+      strip.setPixelColor(0, strip.Color(0, 0, 0));
+      strip.show();
+      // strip.clear();
+      Serial.println(0);
+    }
+  }
+  if (soundVolume == 1)
+  {
+    if (counter < times)
+    {
+      strip.setPixelColor(0, strip.Color(0, 255, 255));
+      strip.show();
+      Serial.println(1);
+    }
+    counter = 0;
+  }
+  if (soundVolume == 2)
+  {
+    if (counter < times)
+    {
+      strip.setPixelColor(0, strip.Color(0, 255, 0));
+      strip.show();
+      Serial.println(2);
+    }
+    counter = 0;
+  }
+  if (soundVolume == 3)
+  {
+    if (counter < times)
+    {
+      strip.setPixelColor(0, strip.Color(0, 0, 255));
+      strip.show();
+      Serial.println(3);
+    }
+    counter = 0;
+  }
+  if (soundVolume == 4)
+  {
+    if (counter < times)
+    {
+      strip.setPixelColor(0, strip.Color(255, 0, 255));
+      strip.show();
+      Serial.println(4);
+    }
+    counter = 0;
+  }
+  if (soundVolume == 5)
+  {
+    if (counter < times)
+    {
+      strip.setPixelColor(0, strip.Color(255, 255, 0));
+      strip.show();
+      Serial.println(5);
+    }
+    counter = 0;
+  }
+  if (soundVolume >= 6)
+  {
+    if (counter < times)
+    {
+      strip.setPixelColor(0, strip.Color(255, 0, 0));
+      strip.show();
+      Serial.println(6);
+    }
+    counter = 0;
+  }
+
+  delay(60);
 }
